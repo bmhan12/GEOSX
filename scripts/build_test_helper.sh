ffff515b004ad47fe4edbd7367fe0f3b3cc0af7a
Brian Han <han12@llnl.gov>
2022-08-16 12:55:34 -0700

    Build geosx target one rank

--- scripts/build_test_helper.sh
+++ scripts/build_test_helper.sh
@@ -68,10 +68,7 @@ fi
 
 # "Make" target check (builds geosx executable target only if true)
 if [[ "$*" == *--build-exe-only* ]]; then
-  or_die make -j $(nproc) mesh
-  or_die make -j 1 constitutive
-  or_die make -j 1 physicsSolvers 
-  or_die make -j $(nproc) geosx VERBOSE=1
+  or_die make -j 1 geosx VERBOSE=1
 else
   or_die make -j $(nproc) VERBOSE=1
   or_die make install VERBOSE=1
