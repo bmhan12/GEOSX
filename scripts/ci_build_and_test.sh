9cc5e12dd6fc38e5b48d2731f441576b6c8220dd
Brian Manh Hien Han <han12@llnl.gov>
2023-04-14 12:25:23 -0700

    peek around

--- scripts/ci_build_and_test.sh
+++ scripts/ci_build_and_test.sh
@@ -40,6 +40,6 @@ docker run \
 -e ENABLE_HYPRE_CUDA=${ENABLE_HYPRE_CUDA:-OFF} \
 -e ENABLE_TRILINOS=${ENABLE_TRILINOS:-ON} \
 ${DOCKER_REPOSITORY}:${GEOSX_TPL_TAG} \
-echo dog;
+ls;
 # ${BUILD_DIR_MOUNT_POINT}/scripts/travis_build_and_test.sh ${BUILD_AND_TEST_ARGS};
 
